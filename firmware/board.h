e93b8be315b76f835f933d07b40a7bcb19982800
bgomberg <bgomberg@getpebble.com>
2015-07-27 19:41:18 -0700

    Flat

--- firmware/board.h
+++ firmware/board.h
@@ -160,12 +160,12 @@ void USART_HalfDuplexCmd(USART_TypeDef* USARTx, FunctionalState NewState)
   if (NewState != DISABLE)
   {
     /* Enable the Half-Duplex mode by setting the HDSEL bit in the CR3 register */
-    USARTx->CR3 |= USART_CR3_HDSEL;
+    USARTx->CR3 |= 0x0008;
   }
   else
   {
     /* Disable the Half-Duplex mode by clearing the HDSEL bit in the CR3 register */
-    USARTx->CR3 &= (uint16_t)~((uint16_t)USART_CR3_HDSEL);
+    USARTx->CR3 &= (uint16_t)~((uint16_t)0x0008);
   }
 }
 
