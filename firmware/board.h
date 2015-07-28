de88351c57906f1935807b1889c2f5f6c50af85e
bgomberg <bgomberg@getpebble.com>
2015-07-27 17:11:16 -0700

    Updated board.h

--- firmware/board.h
+++ firmware/board.h
@@ -10,19 +10,34 @@
 
 #define BOARD_SERIAL Serial1
 static inline void board_set_tx_enabled(bool enabled) {
-  if (enabled) {
-    USART_ITConfig(USART2, USART_IT_TXE, ENABLE);
-    USART_ITConfig(USART2, USART_IT_RXE, DISABLE);
-  } else {
-    USART_ITConfig(USART2, USART_IT_TXE, ENABLE);
-    USART_ITConfig(USART2, USART_IT_RXE, DISABLE);
-#if 0
-    bitClear(UCSR1B, TXEN1);
-    bitClear(DDRD, 3);
-    bitSet(PORTD, 3);
-    bitSet(UCSR1B, RXEN1);
-#endif
-  }
+  // USART default configuration
+  // USART configured as follow:
+  // - BaudRate = (set baudRate as 9600 baud)
+  // - Word Length = 8 Bits
+  // - One Stop Bit
+  // - No parity
+  // - Hardware flow control disabled (RTS and CTS signals)
+  // - Receive and transmit enabled
+  USART_InitStructure.USART_BaudRate = 9600;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+
+  if (enabled)
+    USART_InitStructure.USART_Mode = USART_Mode_Tx;
+  else
+    USART_InitStructure.USART_Mode = USART_Mode_Rx;
+
+  // Configure USART
+  USART_Init(usartMap->usart_peripheral, &USART_InitStructure);
+
+  // Enable USART Receive and Transmit interrupts
+  USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+  USART_ITConfig(USART2, USART_IT_TXE, ENABLE);
+
+  // Enable the USART
+  USART_Cmd(USART2, ENABLE);
 }
 static inline void board_set_even_parity(bool enabled) {
 #if 0
