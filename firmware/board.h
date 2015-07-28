70a98e257e8417fb299e266870a12c13d6d1a866
bgomberg <bgomberg@getpebble.com>
2015-07-27 18:59:49 -0700

    Flat

--- firmware/board.h
+++ firmware/board.h
@@ -158,8 +158,6 @@ void USART_Cmd(USART_TypeDef* USARTx, FunctionalState NewState)
 static bool tx_enabled = false;
 static bool parity = false;
 static void do_update(void) {
-  pinMode(TX, AF_OUTPUT_DRAIN);
-  pinMode(RX, INPUT_PULLUP);
   // USART default configuration
   // USART configured as follow:
   // - BaudRate = (set baudRate as 9600 baud)
